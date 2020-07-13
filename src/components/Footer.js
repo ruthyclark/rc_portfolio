39178e859ae73e9a4f028112208b2fce52445b7d
Ruthless <ruth@jetpackworkflow.com>
2020-07-12 22:50:50 -0400

    Added email icon in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,15 @@ function Footer() {
             <Container fluid={true}>
                 <Row className='border-top justify-content-between p-3'>
                     <Col className='p-0' md={3} sm={12}>
-                    <a className= "btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/ruth-a-clark">
-                        <i className="fa fa-2x fa-linkedin-square" aria-hidden="true"></i></a>
-                    <a className= "btn btn-social-icon btn-github" href="https://github.com/ruthyclark"> 
-                        <i className="fa fa-2x fa-github-square"></i></a>
+                        <a className= "btn btn-social-icon btn-linkedin" href="https://www.linkedin.com/in/ruth-a-clark">
+                            <i className="fa fa-2x fa-linkedin-square" aria-hidden="true"></i>
+                        </a>
+                        <a className= "btn btn-social-icon btn-github" href="https://github.com/ruthyclark"> 
+                            <i className="fa fa-2x fa-github-square" aria-hidden="true"></i>
+                        </a>
+                        <a className= "btn btn-social-icon btn-email" href="mailto:ruthclark2527@gmail.com?subject=I'm%20not%20Ruthless">
+                            <i class="fa fa-2x fa-envelope" aria-hidden="true"></i>
+                        </a>
                     </Col>
                     <Col className='p-0 d-flex justify-content-end' md={3}>
                     Don't be Ruth-less
